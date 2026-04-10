7321b92dc460f8dd0a2f92262769029e064e2355
BartoszBee <142789153+BartoszBee@users.noreply.github.com>
2026-04-10 20:22:44 +0200

    register route - without validation

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,15 +1,36 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-import sql from "@/lib/db"
+import sql from "@/lib/db";
+import bcrypt from "bcryptjs";
 
 export async function POST(request: NextRequest) {
+  try {
     const { email, password } = await request.json();
 
     const result = await sql`
-      SELECT * FROM users 
+      SELECT * FROM users
       WHERE email = ${email}
-    `
+    `;
 
-    console.log(result)
+    if (result.length > 0) {
+      return NextResponse.json(
+        { error: "Podany email już istnieje - zaloguj się" },
+        { status: 409 },
+      );
+    }
 
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await sql`
+      INSERT INTO users (email, password_hash) VALUES (${email},${hashedPassword}) RETURNING id,email      
+    `;
+
+    return NextResponse.json(newUser[0], { status: 201 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Wewnętrzny błąd serwera" },
+      { status: 500 },
+    );
+  }
 }
